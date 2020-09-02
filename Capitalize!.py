17f68a631d23d6827fa1f5afa19f23c0ee69bf7d
Amandeep Singh <47387270+Amandeepanmol@users.noreply.github.com>
2020-09-02 22:41:00 +0530

    Create Capitalize!.py

---
+++ Capitalize!.py
@@ -0,0 +1,15 @@
+import string
+def solve(s):
+    p=string.capwords(s,' ')
+    return p
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
