13ffcd8cb307be249f4b7128c39168076747f9fc
Amandeep Singh <47387270+Amandeepanmol@users.noreply.github.com>
2020-09-04 22:12:37 +0530

    Create Set _intersection().py

---
+++ Set _intersection().py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n=int(input())
+eng=set(map(int,input().strip().split()))
+
+b=int(input())
+fre=set(map(int,input().strip().split()))
+
+res=eng.intersection(fre)
+print(len(res))
